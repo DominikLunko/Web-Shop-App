88dc575173a9a758f6715da67a2bfa68b1ea744f
Dominik <dlunko@foi.hr>
2021-07-25 22:13:52 +0200

    changed Port

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 server.use(middlewares)
 server.use(router)
